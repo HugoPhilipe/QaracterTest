bd3f00d695d4887fbf77f67b6efa258b37a9b8b6
Hugo Philipe <lipy_hugo@live.com>
2025-06-06 02:22:16 -0300

    config main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,14 +22,15 @@ resource "azurerm_container_app_environment" "env" {
 }
 
 resource "azurerm_container_app" "app" {
-  name                         = var.container_app_name
+  name                         = "spring-hello-app"
   container_app_environment_id = azurerm_container_app_environment.env.id
   resource_group_name          = var.resource_group
+  revision_mode                = "Single"  # ou "Multiple" se você quiser controle de revisões
 
   template {
     container {
-      name   = "spring"
-      image  = var.container_image
+      name   = "spring-hello"
+      image  = "${var.acr_name}.azurecr.io/${var.image_name}:latest"
       cpu    = 0.5
       memory = "1.0Gi"
     }
@@ -38,5 +39,15 @@ resource "azurerm_container_app" "app" {
   ingress {
     external_enabled = true
     target_port      = 8080
+
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 }
+
