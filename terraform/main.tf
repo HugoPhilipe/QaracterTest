2fa7a1f5b65d7b7f98ccf74e5b01e63c7d7d9c97
Hugo Philipe <lipy_hugo@live.com>
2025-06-07 18:38:29 -0300

    config terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,11 +26,33 @@ resource "azurerm_container_app_environment" "env" {
   resource_group_name = var.resource_group
 }
 
+resource "azurerm_user_assigned_identity" "app" {
+  name                = "spring-hello-app-identity"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_user_assigned_identity.app.principal_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
+
 resource "azurerm_container_app" "app" {
   name                         = var.container_app_name
   container_app_environment_id = azurerm_container_app_environment.env.id
   resource_group_name          = var.resource_group
   revision_mode                = "Single"  # ou "Multiple" se você quiser controle de revisões
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.app.id]
+  }
+
+  registry {
+    server   = azurerm_container_registry.acr.login_server
+    identity = azurerm_user_assigned_identity.app.id
+  }
 
   template {
     container {
@@ -49,10 +71,6 @@ resource "azurerm_container_app" "app" {
       percentage      = 100
       latest_revision = true
     }
-  }
-
-  identity {
-    type = "SystemAssigned"
   }
 }
 
