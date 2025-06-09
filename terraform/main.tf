ff7408b22cce7535a9b42eec8ed5fe9be951c43a
Hugo Philipe <lipy_hugo@live.com>
2025-06-09 10:07:57 -0300

    comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Configura o provedor AzureRM com as credenciais necessárias
 provider "azurerm" {
   features {}
 
@@ -12,6 +13,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+# Cria um Azure Container Registry (ACR) para armazenar imagens de container
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = var.resource_group
@@ -20,24 +22,28 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
+# Cria um ambiente para Container Apps no Azure
 resource "azurerm_container_app_environment" "env" {
   name                = "spring-hello-env"
   location            = var.location
   resource_group_name = var.resource_group
 }
 
+# Cria uma identidade gerenciada do usuário para o app
 resource "azurerm_user_assigned_identity" "app" {
   name                = "spring-hello-app-identity"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
+# Concede permissão AcrPull para a identidade acessar
 resource "azurerm_role_assignment" "acr_pull" {
   principal_id         = azurerm_user_assigned_identity.app.principal_id
   role_definition_name = "AcrPull"
   scope                = azurerm_container_registry.acr.id
 }
 
+# Cria o Container App no Azure, usando o ambiente, identidade e ACR definidos acima
 resource "azurerm_container_app" "app" {
   name                         = var.container_app_name
   container_app_environment_id = azurerm_container_app_environment.env.id
@@ -63,6 +69,7 @@ resource "azurerm_container_app" "app" {
     }
   }
 
+# Configura o acesso externo e a porta do app
   ingress {
     external_enabled = true
     target_port      = 8080
